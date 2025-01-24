57297c11f2db9a1d04efac7c8cdd0b660aaa60ae
Not-A-Builder <rb1311997@gmail.com>
2025-01-24 20:05:46 +0530

    Initial Commit

---
+++ Contemplaunce/ContemplaunceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContemplaunceApp.swift
+//  Contemplaunce
+//
+//  Created by Rounak Bose on 24/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ContemplaunceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContemplaunceTests/ContemplaunceTests.swift
@@ -0,0 +1,17 @@
+//
+//  ContemplaunceTests.swift
+//  ContemplaunceTests
+//
+//  Created by Rounak Bose on 24/01/25.
+//
+
+import Testing
+@testable import Contemplaunce
+
+struct ContemplaunceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
