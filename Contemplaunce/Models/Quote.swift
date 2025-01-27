4911b4a423d178d62aca204bf4b1243e19d70f4a
Not-A-Builder <rb1311997@gmail.com>
2025-01-27 19:45:14 +0530

    updated dataset

--- Contemplaunce/Models/Quote.swift
+++ Contemplaunce/Models/Quote.swift
@@ -17,5 +17,9 @@ extension Quote {
         )
     }
     
-    static var previewQuotes: [Quote] = []
+    static var previewQuotes: [Quote] = [
+        Quote(text: "The happiness of your life depends upon the quality of your thoughts.", author: "Marcus Aurelius", date: Date()),
+        Quote(text: "Waste no more time arguing about what a good man should be. Be one.", author: "Marcus Aurelius", date: Date()),
+        Quote(text: "The best revenge is to be unlike him who performed the injury.", author: "Marcus Aurelius", date: Date())
+    ]
 } 
